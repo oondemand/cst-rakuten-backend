4eecc86136d865ab8c2809799d7853e6d3ca5768
Maike <maikonalexandre574@gmail.com>
2025-01-08 13:41:14 -0300

    update v1.1.2

--- src/routers/statusRouter.js
+++ src/routers/statusRouter.js
@@ -14,7 +14,7 @@ router.get("/", async (req, res) => {
   };
 
   res.status(200).json({
-    message: `${process.env.SERVICE_NAME} rodando - vs 1.1.5`,
+    message: `${process.env.SERVICE_NAME} rodando - vs 1.2.0`,
     database: status[dbState]
       ? `${status[dbState]} em ${process.env.DB_NAME}`
       : "Status desconhecido",
