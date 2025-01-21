078f72b3101be3b3940c1bec4ea2cd2cb17593a8
Maike <maikonalexandre574@gmail.com>
2025-01-21 15:51:19 -0300

    feat: update Docker workflow for dynamic versioning and release management

--- src/routers/statusRouter.js
+++ src/routers/statusRouter.js
@@ -14,7 +14,7 @@ router.get("/", async (req, res) => {
   };
 
   res.status(200).json({
-    message: `${process.env.SERVICE_NAME} rodando - vs 1.2.1`,
+    message: `${process.env.SERVICE_NAME} rodando - vs ${process.env.SERVICE_VERSION}`,
     database: status[dbState]
       ? `${status[dbState]} em ${process.env.DB_NAME}`
       : "Status desconhecido",
