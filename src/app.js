08dba0de83ed4dde7cf468d0ccbaad3dfe060522
Maike <maikonalexandre574@gmail.com>
2025-03-28 08:48:05 -0300

    fix: increase JSON and URL-encoded body size limits in app middleware

--- src/app.js
+++ src/app.js
@@ -17,7 +17,8 @@ const rastreabilidadeMiddleware = require("./middlewares/rastreabilidadeMiddlewa
 const app = express();
 
 // Middlewares globais
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cors({ origin: "*" }));
 app.use(helmet());
 app.use(express.static(path.join(__dirname, "public")));
