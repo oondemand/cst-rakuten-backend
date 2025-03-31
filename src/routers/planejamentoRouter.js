d3613ca13c54bc4fc69b7013ebc9f5246ef5d2aa
Maike <maikonalexandre574@gmail.com>
2025-03-31 11:03:09 -0300

    feat: add estatisticas endpoint for service statistics aggregation

--- src/routers/planejamentoRouter.js
+++ src/routers/planejamentoRouter.js
@@ -5,10 +5,12 @@ const {
 const {
   listarServicos,
 } = require("../controllers/planejamento/listarServicos");
+const { estatisticas } = require("../controllers/planejamento/estatisticas");
 
 const router = express.Router();
 
 router.get("/listar-servicos", listarServicos);
+router.get("/estatisticas", estatisticas);
 router.post("/sincronizar-esteira", sincronizarEsteira);
 
 module.exports = router;
